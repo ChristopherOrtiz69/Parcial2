a720646ad2672864ace395740fda2873d02408aa
ChristopherOrtiz69 <christopher.villafanet@gmail.com>
2023-10-02 17:33:49 -0600

    cambio

---
+++ practica7.js
@@ -0,0 +1,18 @@
+function factorial(){
+
+    n=prompt('Ingrese un numero:','');
+
+    parseInt(n, 10)
+    
+    let resultado=1;
+    
+    for(let i = n;i>=1;i--){
+
+        resultado *= i;
+        
+    }
+
+    return resultado;
+}
+
+console.log(factorial());
